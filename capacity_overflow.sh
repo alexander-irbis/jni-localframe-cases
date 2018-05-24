cbace65ab73b6f5e7a2c0b45b9f120aee02bf55b
Alexander Irbis <irbis.labs@gmail.com>
2018-05-24 14:28:15 +0300

    Init

---
+++ capacity_overflow.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export RUST_LIB_DIR=$(rustup run stable rustc --print sysroot)/lib
+export JAVA_HOME="${JAVA_HOME:-$(mvn --version | grep 'Java home' | sed 's/.*: //')}"
+export JAVA_LIB_DIR="$(find ${JAVA_HOME} -type f -name libjvm.\* | xargs -n1 dirname)"
+
+export LD_LIBRARY_PATH=$JAVA_LIB_DIR:$RUST_LIB_DIR
+#export LD_LIBRARY_PATH=/Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home/jre/lib/server/:$RUST_LIB_DIR
+
+env
+
+cargo run --example capacity_overflow
